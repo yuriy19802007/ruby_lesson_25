343c89f1bee29d8c199866870d3d6bc3ef934122
jurij <yura19802007@gmail.com>
2020-10-05 17:33:59 +0300

    Select query

--- app.rb
+++ app.rb
@@ -4,5 +4,12 @@
 require 'sqlite3'
 
 
-db = SQLite3::Database.new 'test.db'
+db = SQLite3::Database.new 'testsql'
+
+db.execute "select * from Cars " do |car|
+    puts car
+    puts "===="
+end
+
+db.close
 
